0ecd00fc2130f5a940902758b2d56077c060169c
Williams David <phenomenon1995@gmail.com>
2024-08-02 13:02:50 -0400

    DDL for PizzaShopDB implemeneted. Handing off, my fingies hurt.

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,6 +1,28 @@
-BEGIN TRANSACTION;
+--BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-COMMIT TRANSACTION;
+INSERT INTO pizza_size (pizza_size_id, pizza_size_description, diameter, base_price) VALUES ('S', 'Small', 12, 12.99);
+INSERT INTO pizza_size (pizza_size_id, pizza_size_description, diameter, base_price) VALUES ('M', 'Medium', 14, 14.99);
+INSERT INTO pizza_size (pizza_size_id, pizza_size_description, diameter, base_price) VALUES ('L', 'Large', 16, 16.99);
+INSERT INTO pizza_size (pizza_size_id, pizza_size_description, diameter, base_price) VALUES ('XL' , 'Extra Large', 18, 18.99);
+
+INSERT INTO topping (topping_name, additional_price) VALUES ('Pepperoni', 1.25);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Sausage', 1.25);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Extra Cheese', 0.75);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Green Peppers', 1.00);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Parmesean', 0.75);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Onions', 1.00);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Mushrooms', 1.00);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Chicken', 1.00);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Bacon', 1.25);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Anchovies', 2.00);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Black Olives', 0.50);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Pineapple', 1.00);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Ham', 1.25);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Tomatoes', 0.50);
+INSERT INTO topping (topping_name, additional_price) VALUES ('Jalapenos', 0.50);
+
+
+--COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,116 @@
-BEGIN TRANSACTION;
+--BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS customer CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS specialty_pizza CASCADE;
+DROP TABLE IF EXISTS pizza CASCADE;
+DROP TABLE IF EXISTS drink;
+DROP TABLE IF EXISTS pizza_topping CASCADE;
+DROP TABLE IF EXISTS topping CASCADE;
+DROP TABLE IF EXISTS pizza_size CASCADE;
+DROP TABLE IF EXISTS drink_size CASCADE;
+
+
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
+	role varchar(50) NOT NULL
 );
 
-COMMIT TRANSACTION;
+CREATE TABLE customer(
+	customer_id SERIAL,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	street_address varchar(50),
+	city varchar(50),
+	zip_code numeric(5,0),
+	state_abbreviation char(2),
+	phone_number numeric(10,0) NOT NULL,
+	email varchar(50) NOT NULL,	
+	CONSTRAINT PK_customer_id  PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE orders (
+	order_id SERIAL,
+	customer_id int NOT NULL, 
+	total numeric(5,2) NOT NULL,
+	is_delivery boolean NOT NULL,
+	CONSTRAINT PK_order_id PRIMARY KEY (order_id)
+);
+CREATE TABLE specialty_pizza (
+	specialty_pizza_id SERIAL,
+	specialty_pizza_name varchar(50) NOT NULL,
+	crust varchar(20) NOT NULL, 
+	sauce varchar(20) NOT NULL,
+	premium numeric (4,2)
+	
+	-- SELECT ... SUM(sp.premium, ps.base_price) AS price FROM specialty_pizza sp
+	-- JOIN pizza_size ps ON sp.specialty_pizza_id = ps.specialty_pizza_id
+	-- WHERE sp.specialty_pizza_id = ?
+	
+);
+
+CREATE TABLE pizza (
+	pizza_id SERIAL,
+	order_id int NOT NULL,
+	size_id varchar(2) NOT NULL,
+	pizza_name varchar(20),
+	crust varchar(20) NOT NULL, 
+	sauce varchar(20) NOT NULL,
+	price numeric (4,2) NOT NULL,
+	additional_instruction varchar(200), 
+	CONSTRAINT PK_pizza_id PRIMARY KEY (pizza_id)
+);
+
+CREATE TABLE drink(
+	drink_id SERIAL, 
+	drink_size_id int NOT NULL,
+	flavor varchar(50) NOT NULL,
+	price numeric (3,2) NOT NULL, 
+	CONSTRAINT PK_drink_id PRIMARY KEY (drink_id)
+);
+
+
+CREATE TABLE topping(
+	topping_name varchar(50) NOT NULL,
+	additional_price numeric(3,2), 
+	CONSTRAINT PK_topping_name PRIMARY KEY (topping_name)
+);
+CREATE TABLE pizza_size (
+	pizza_size_id varchar(2),
+	pizza_size_description varchar (20) NOT NULL, 
+	diameter numeric(2,0),
+	base_price numeric (4,2), 
+	CONSTRAINT PK_pizza_size_id PRIMARY KEY (pizza_size_id)
+);
+CREATE TABLE drink_size(
+	drink_size_id SERIAL,
+	drink_size_description varchar (10) NOT NULL, 
+	drink_size_price numeric(4,2), 
+	CONSTRAINT PK_drink_size_id PRIMARY KEY (drink_size_id)
+);
+
+CREATE TABLE pizza_topping (
+	pizza_id int NOT NULL,
+	topping_name varchar(50) NOT NULL,
+	CONSTRAINT FK_pizza_topping_pizza FOREIGN KEY (pizza_id) REFERENCES pizza(pizza_id),
+	CONSTRAINT FK_pizza_topping_topping FOREIGN KEY (topping_name) REFERENCES topping(topping_name)
+);
+
+ALTER TABLE pizza 
+	ADD CONSTRAINT FK_pizza_order FOREIGN KEY (order_id) REFERENCES orders(order_id),
+	ADD CONSTRAINT FK_pizza_size FOREIGN KEY (size_id) REFERENCES pizza_size(pizza_size_id)
+	
+;
+AlTER TABLE orders 
+	ADD CONSTRAINT FK_orders_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+;
+
+ALTER TABLE drink 
+	ADD CONSTRAINT FK_drink_size FOREIGN KEY (drink_size_id) REFERENCES drink_size(drink_size_id)
+;
+
+--ROLLBACK TRANSACTION;
